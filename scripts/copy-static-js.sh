5b14926e5ef130399a7f2091e05a30f711a26399
toshusai <35446607+toshusai@users.noreply.github.com>
2021-08-27 00:08:15 +0900

    Fix depended ccapture.js.

--- scripts/copy-static-js.sh
+++ scripts/copy-static-js.sh
@@ -1,3 +1,9 @@
 # Copy Static JavaScript Libraries
 cp node_modules/@ffmpeg/core/dist/* static/static/js/ffmpeg
-cp node_modules/ccapture.js/build/CCapture.all.min.js static/static/js/ccapture.js
+cp node_modules/ccapture.js/build/CCapture.all.min.js static/static/js/ccapture.js/CCapture.all.min.js
+
+
+sed 's/try{Object.defineProperty(HTMLVideoElement.prototype,"currentTime",{get:t}),Object.defineProperty(HTMLAudioElement.prototype,"currentTime",{get:t})}catch(t){b(t)}//' \
+    static/static/js/ccapture.js/CCapture.all.min.js > tmp
+cat tmp > static/static/js/ccapture.js/CCapture.all.min.js
+rm tmp
