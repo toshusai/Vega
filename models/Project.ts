9d1d274ebe9ebced38d6132e3ad88adf5a73f76a
toshusai <35446607+toshusai@users.noreply.github.com>
2021-09-01 01:05:16 +0900

    Add Project type guard.

--- models/Project.ts
+++ models/Project.ts
@@ -12,3 +12,36 @@ export interface Project {
   assets: (IAsset | IAudioAsset | IVideoAsset | IFontAsset)[];
   strips: (IStrip | IVideoStrip | ITextStrip)[];
 }
+
+export function isProject(input: any): input is Project {
+  return (
+    typeof input.version == "string" &&
+    typeof input.name == "string" &&
+    typeof input.width == "number" &&
+    typeof input.height == "number" &&
+    typeof input.fps == "number" &&
+    typeof input.duration == "number" &&
+    Array.isArray(input.assets) &&
+    input.assets.every((v: any) => isAsset(v)) &&
+    Array.isArray(input.strips) &&
+    input.assets.every((v: any) => isStrip(v))
+  );
+}
+
+export function isAsset(input: any): input is IAsset {
+  return (
+    typeof input.id == "string" &&
+    typeof input.name == "string" &&
+    typeof input.type == "string"
+  );
+}
+
+export function isStrip(input: any): input is IStrip {
+  return (
+    typeof input.id == "string" &&
+    typeof input.start == "number" &&
+    typeof input.length == "number" &&
+    typeof input.layer == "number" &&
+    typeof input.type == "string"
+  );
+}
