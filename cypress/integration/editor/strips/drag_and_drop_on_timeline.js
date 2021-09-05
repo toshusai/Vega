8a4947bf5a53215b967373d349601966021c0b4b
toshusai <35446607+toshusai@users.noreply.github.com>
2021-09-06 00:38:10 +0900

    Add more asset file tests.

---
+++ cypress/integration/editor/strips/drag_and_drop_on_timeline.js
@@ -0,0 +1,19 @@
+describe("Add VideoStrip by D&D on Timeline", () => {
+  it("successfully drag and drop on Timeline", () => {
+    cy.visit("/editor");
+    cy.wait(1000);
+    cy.fixture("assets/bun33s.mp4", "binary")
+      .then(Cypress.Blob.binaryStringToBlob)
+      .then((fileContent) => {
+        cy.get("[data-vega-timeline-container]").attachFile(
+          {
+            fileContent,
+            fileName: "bun33s.mp4",
+            mimeType: "video/mp4",
+            lastModified: new Date().getTime(),
+          },
+          { subjectType: "drag-n-drop" }
+        );
+      });
+  });
+});
