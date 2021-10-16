5218d561924a0c72e9ac77e8c6d6bcb1c9c34892
toshusai <toshusai.net@gmail.com>
2021-10-16 19:29:56 +0900

    Add isSupportMediaType methods.

--- src/renderer/models/assets/ImageAsset.ts
+++ src/renderer/models/assets/ImageAsset.ts
@@ -2,7 +2,13 @@ import { Asset, IAsset } from "./Asset";
 
 export type IImageAsset = IAsset;
 
+const supportTypes = ["image/png", "image/jpg", "image/jpeg"];
+
 export class ImageAsset extends Asset implements IImageAsset {
   type: string = "Image";
   valid: boolean = false;
+
+  public static isSupportType(type: string) {
+    return supportTypes.includes(type);
+  }
 }
