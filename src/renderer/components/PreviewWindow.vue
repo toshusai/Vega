0c7199167d15f6ae01827823681db27d23836b7b
toshusai <35446607+toshusai@users.noreply.github.com>
2021-11-21 21:17:26 +0900

    Fix preview time format

--- src/renderer/components/PreviewWindow.vue
+++ src/renderer/components/PreviewWindow.vue
@@ -152,7 +152,7 @@ export default class PreviewWindow extends Vue {
     const ms = sec.substr(sec.length - 4, 4);
     const hhmmss = new Date(s * 1000).toISOString().substr(11, 8);
 
-    let sign = " ";
+    let sign = "";
     if (this.currentTime < 0) sign = "-";
     return `${sign}${hhmmss}.${ms}`;
   }
