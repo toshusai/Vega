d96b305e2baf18b5e786594e7c1d58246028cd34
toshusai <35446607+toshusai@users.noreply.github.com>
2021-11-21 21:18:49 +0900

    Fix all tests

--- cypress/integration/editor/open_project/migration_v0_0_3.js
+++ cypress/integration/editor/open_project/migration_v0_0_3.js
@@ -1,7 +1,7 @@
 describe("Open v0.0.3 Project File", () => {
   it("successfully migration TextStrip to Text3DStrip", () => {
     cy.visit("/editor");
-    cy.contains("Project").click();
+    cy.get("[data-project-action-menu]").click();
     cy.contains("Open Project").click();
     cy.get("input").attachFile("project_files/v0_0_3.json");
 
