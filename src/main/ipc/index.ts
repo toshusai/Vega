6b160b16e345b334bd32d806eedbf9a9f1e1e0ab
toshusai <35446607+toshusai@users.noreply.github.com>
2021-11-21 12:18:43 +0900

    Add @toshusai/spectrum-vue

---
+++ src/main/ipc/index.ts
@@ -0,0 +1,10 @@
+// import { ipcMain } from "electron";
+
+// ipcMain.on("")
+
+/**
+ *       const strip = this.strips[i];
+      if (StripUtil.isThreeJsStrip(strip)) {
+        strip.obj.remove();
+
+ */
---
+++ src/renderer/plugins/register.ts
@@ -0,0 +1,6 @@
+import * as components from "@toshusai/spectrum-vue"
+import Vue from "vue"
+
+Object.entries(components).forEach(([name, component]) => {
+    Vue.component(name, component)
+})
