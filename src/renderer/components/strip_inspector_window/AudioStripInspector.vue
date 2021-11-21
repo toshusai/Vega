a405b0a54e02f73e6ea297e54f5c8e1749be8070
toshusai <toshusai.net@gmail.com>
2021-11-21 20:31:00 +0900

    Update AudioStripInspector

--- src/renderer/components/strip_inspector_window/AudioStripInspector.vue
+++ src/renderer/components/strip_inspector_window/AudioStripInspector.vue
@@ -1,33 +1,20 @@
 <template>
   <div style="padding: 4px">
     <sp-field-label> Start </sp-field-label>
-    <sp-textfield
-      size="S"
-      type="number"
-      :step="0.01"
-      :value="strip.start"
-      @change="changeStart"
-    />
+    <sp-textfield v-model="strip.start" size="S" type="number" :step="0.01" />
 
     <sp-field-label> Offset </sp-field-label>
     <sp-textfield
+      v-model="strip.videoOffset"
       size="S"
       type="number"
       :step="0.01"
-      :value="strip.videoOffset"
-      @change="changeVideoOffset"
     />
 
     <sp-field-label>Length</sp-field-label>
-    <sp-textfield
-      size="S"
-      type="number"
-      :step="0.01"
-      :value="strip.length"
-      @change="changeLength"
-    />
+    <sp-textfield v-model="strip.length" size="S" type="number" :step="0.01" />
 
-    <sp-field-label>Video</sp-field-label>
+    <sp-field-label>Audio</sp-field-label>
     <VegaSelect
       :items="selectItems"
       :value="currentAssetId"
@@ -46,13 +33,46 @@
 <script lang="ts">
 import Vue from "vue";
 import { Component, Prop, PropSync } from "vue-property-decorator";
-import { Asset, AudioStrip } from "~/models";
+import { OptionKeyValue } from "../vega/VegaSelect.vue";
+import { Asset, AudioStrip, AudioAsset } from "~/models";
+import VegaSelect from "~/components/vega/VegaSelect.vue";
 
-@Component({})
+@Component({
+  components: {
+    VegaSelect,
+  },
+})
 export default class AudioStripInspector extends Vue {
   @PropSync("stripSync") strip!: AudioStrip;
 
-  @Prop({ default: () => [] })
-  assets!: Asset[];
+  @Prop({ default: () => [] }) assets!: Asset[];
+
+  get currentAssetId() {
+    return this.strip.videoAsset?.id;
+  }
+
+  get audioAssets(): AudioAsset[] {
+    return this.assets.filter((a) => a instanceof AudioAsset) as AudioAsset[];
+  }
+
+  get selectItems() {
+    const items: OptionKeyValue[] = [
+      { value: "", text: "No selected", disabled: true },
+    ];
+    return items.concat(
+      this.audioAssets.map((va: AudioAsset) => {
+        return {
+          value: va.id,
+          text: va.name,
+        };
+      })
+    );
+  }
+
+  changeSrc(e: OptionKeyValue) {
+    const targetAsset = this.assets.find((a) => a.id == e.value);
+    if (!targetAsset || !(targetAsset instanceof AudioAsset)) return;
+    this.strip.updateAsset(targetAsset);
+  }
 }
 </script>
