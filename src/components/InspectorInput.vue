91eb02cae179ca8d0cf405fbc199af58d02eca05
toshusai <35446607+toshusai@users.noreply.github.com>
2022-05-28 10:48:36 +0900

    edit keyframe in inspector

--- src/components/InspectorInput.vue
+++ src/components/InspectorInput.vue
@@ -1,13 +1,34 @@
 <script setup lang="ts">
 import InspectorInputBase from './InspectorInputBase.vue'
-defineProps<{ label: string; value: number }>()
-const emit = defineEmits<{(e: 'input', value: number): void; }>()
+
+defineProps<{ label: string; value: number, keyFrame?: boolean }>()
+
+const emit = defineEmits<{(e: 'input', value: number): void; (e: 'key-frame'): void }>()
+
 function update (e: number) {
   emit('input', e)
 }
+
 </script>
 <template>
   <inspector-input-base :label="label">
     <v-input v-bind="$attrs" :value="value" @input="update" />
+    <svg
+      viewBox="0 0 24 24"
+      class="w-[24px] h-[24px]  cursor-pointer select-none"
+      @click="emit('key-frame')"
+    >
+      <path
+        v-if="!keyFrame"
+        fill="currentColor"
+        d="M12 2C11.5 2 11 2.19 10.59 2.59L2.59 10.59C1.8 11.37 1.8 12.63 2.59 13.41L10.59 21.41C11.37 22.2 12.63 22.2 13.41 21.41L21.41 13.41C22.2 12.63 22.2 11.37 21.41 10.59L13.41 2.59C13 2.19 12.5 2 12 2M12 4L20 12L12 20L4 12Z"
+      />
+      <path
+        v-else
+        fill="orange"
+        d="M12 2C11.5 2 11 2.19 10.59 2.59L2.59 10.59C1.8 11.37 1.8 12.63 2.59 13.41L10.59 21.41C11.37 22.2 12.63 22.2 13.41 21.41L21.41 13.41C22.2 12.63 22.2 11.37 21.41 10.59L13.41 2.59C13 2.19 12.5 2 12 2Z"
+      />
+
+    </svg>
   </inspector-input-base>
 </template>
