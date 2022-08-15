c178c19b19adb3123042805ec497193590717b2e
toshusai <35446607+toshusai@users.noreply.github.com>
2022-08-15 11:32:23 +0900

    add asssts error

---
+++ src/components/AssetListItemErrorIcon.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { mdiAlertCircle } from '@mdi/js'
+</script>
+
+<template>
+  <v-icons
+    :path="mdiAlertCircle"
+    fill="red"
+    style="width: 16px; height: 16px; position: absolute; top: 2px; right: 2px; background: white; border-radius: 20px;"
+  />
+</template>
+
+<style scoped>
+</style>
--- src/components/ImageAssetListItem.vue
+++ src/components/ImageAssetListItem.vue
@@ -1,14 +1,31 @@
 <script setup lang="ts">
 import { mdiFileImage } from '@mdi/js'
 import { IAsset } from '../core/IAsset'
+import AssetListItemErrorIcon from './AssetListItemErrorIcon.vue'
 
-defineProps<{ asset: IAsset }>()
+const props = defineProps<{ asset: IAsset }>()
+
+const img = ref<HTMLImageElement | null>(null)
+
+const error = ref<boolean>(false)
+
+onMounted(() => {
+  if (img.value) {
+    img.value.src = props.asset.path
+    img.value.onerror = () => {
+      error.value = true
+    }
+  }
+})
+
 </script>
 
 <template>
-  <div style="display: flex; min-width: 32px">
+  <div style="display: flex; min-width: 32px; position: relative;">
     <v-icons style="margin: auto" fill="white" :path="mdiFileImage" />
+    <asset-list-item-error-icon v-if="error" />
   </div>
+  <img ref="img" style="display: none;">
   <div style="margin: auto 0">
     {{ asset.name }}
   </div>
