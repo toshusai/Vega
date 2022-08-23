75874750fd145639a18f803d2ba351499815fd96
toshusai <toshusai.net@gmail.com>
2022-08-24 00:42:04 +0900

    fix style

---
+++ src/components/VNumberInput.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps({
+  invalid: {
+    default: false,
+    type: Boolean
+  }
+})
+const emits = defineEmits<{
+  (e: 'input', num: number): void
+}>()
+
+const onInput = (e: Event) => {
+  const el = e.target as HTMLInputElement
+  const num = Number.parseFloat(el.value)
+  if (Number.isNaN(num)) { return }
+  emits('input', num)
+}
+</script>
+<template>
+  <v-input-base v-bind="$attrs" @input="onInput" />
+</template>
