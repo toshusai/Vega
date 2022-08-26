93164e5067f8d7457c5b60386245adf2f14ca2e4
toshusai <35446607+toshusai@users.noreply.github.com>
2022-08-27 08:52:52 +0900

    update top page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,10 +3,10 @@
 </script>
 
 <template>
-  <div>
+  <div style="display: flex; flex-direction: column; height: 100%;">
     <div style="justify-content: center;">
       <div class="thumbnail">
-        <img class="image" src="https://github.com/toshusai/vega/vega/screenshot.png">
+        <img class="image" src="https://toshusai.github.io/vega/vega/screenshot.png">
         <div style="margin: auto; z-index: 1; text-align: center;">
           <h1 style="font-size: 32px">
             Vega
@@ -14,9 +14,13 @@
           <h2>
             a open source video editor
           </h2>
-          <!-- <v-button>
+          <v-button
+            style="background-color: rgba(0,0,0,0.5);"
+            href="https://github.com/toshusai/vega/releases"
+            as="a"
+          >
             Download
-          </v-button> -->
+          </v-button>
         </div>
       </div>
     </div>
@@ -34,6 +38,28 @@
       </div>
       <div class="section">
         <div>It is made with web technology and works on many platforms including Mac, Windows, and Linux.</div>
+        <div>
+          <a href="https://toshusai.github.io/vega/app?demo=true">Demo</a> is here (only Google Chrome 103 or
+          later).
+        </div>
+      </div>
+
+      <div class="section-title">
+        Motivation
+      </div>
+      <div class="section">
+        <div>
+          I see potential in web technology and believe that even video editing software can be created. I
+          aim for this software to replace software such as AviUtil (a free video editing software made in
+          Japan) and After Effects.
+        </div>
+      </div>
+    </div>
+    <div style="margin-top: auto">
+      <div style="height: 32px; padding: 0 12px; display: flex;">
+        <div style="margin-left: auto;">
+          (c)toshusai 2022
+        </div>
       </div>
     </div>
   </div>
@@ -73,7 +99,8 @@
     .image {
         transform: perspective(800px) rotate3d(0, 1, 0, 34deg) scale(1.5);
     }
-    .body{
+
+    .body {
         margin: 12px 12px;
     }
 }
