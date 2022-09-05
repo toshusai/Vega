f4f09767d217cf828722a9be19dae5ea0285e81e
toshusai <35446607+toshusai@users.noreply.github.com>
2022-09-05 22:21:56 +0900

    add plugin asset list item

---
+++ src/components/PluginAssetListItem.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { mdiFileCode } from '@mdi/js'
+import { IAsset } from '../core/IAsset'
+
+const props = defineProps<{ asset: IAsset }>()
+
+const error = ref<boolean>(false)
+
+function checkAsset () {
+  error.value = false
+  // TODO check file exist
+}
+
+onMounted(checkAsset)
+watch(() => props.asset, checkAsset)
+</script>
+
+<template>
+  <v-icons style="margin: auto" fill="white" :path="mdiFileCode" />
+  <asset-list-item-error-icon v-if="error" />
+</template>
