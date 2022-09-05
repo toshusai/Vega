91eb54e86cf794a4ccd79f2daead4c5843af007d
toshusai <toshusai.net@gmail.com>
2022-09-05 22:18:32 +0900

    fix keyframe input

--- src/components/InspectorInput.vue
+++ src/components/InspectorInput.vue
@@ -2,7 +2,7 @@
 import { mdiRhombus, mdiRhombusOutline } from '@mdi/js'
 import InspectorInputBase from './InspectorInputBase.vue'
 
-defineProps<{ label: string; value: number; keyFrame?: boolean }>()
+defineProps<{ label: string; value: number; keyFrame?: boolean; hasKeyFrame?:boolean }>()
 
 const emit = defineEmits<{
   (e: 'input', value: number): void
@@ -26,8 +26,8 @@ function update (e: number) {
         v-if="keyFrame"
         viewBox="0 0 24 24"
         class="icon"
-        :path="keyFrame ? mdiRhombus : mdiRhombusOutline"
-        :fill="keyFrame ? 'orange' : 'currentColor'"
+        :path="hasKeyFrame ? mdiRhombus : mdiRhombusOutline"
+        :fill="hasKeyFrame ? 'orange' : 'currentColor'"
         @click="emit('key-frame')"
       />
     </div>
