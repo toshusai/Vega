69aade1d8d53ae71a6f47184272e5f100b86c1c8
toshusai <toshusai.net@gmail.com>
2023-01-29 23:24:11 +0900

    feat: support multi strip edit

--- src/components/strip_panel/hooks/useUpdateEffect.tsx
+++ src/components/strip_panel/hooks/useUpdateEffect.tsx
@@ -5,13 +5,23 @@ import { makeNewKeyframes } from "@/components/preview_panel/utils/makeNewKeyfra
 import { Ease, KeyFrame, Strip } from "@/packages/types";
 import { Effect } from "@/packages/types/src";
 import { actions } from "@/store/scene";
-import { useCurrentTime, useFps } from "@/store/useSelector";
+import { useCurrentTime, useFps, useSelectedStrip } from "@/store/useSelector";
 import { UndoManager } from "@/UndoManager";
 
 export function useUpdateEffect<T extends Effect>(effect: T, strip: Strip) {
   const currentTime = useCurrentTime();
   const fps = useFps();
   const dispatch = useDispatch();
+  const selectedStrips = useSelectedStrip();
+  const allEffects = selectedStrips.flatMap((s) =>
+    s.effects.filter((e) => e.type === effect.type)
+  );
+  const effectIdToStripMap = new Map(
+    selectedStrips.flatMap((s) =>
+      s.effects.filter((e) => e.type === effect.type).map((e) => [e.id, s])
+    )
+  );
+
   const emit = (partial: Partial<T>) => {
     const newKFs = makeNewKeyframes<T>(
       partial,
@@ -21,20 +31,24 @@ export function useUpdateEffect<T extends Effect>(effect: T, strip: Strip) {
       fps
     );
     const redo = () => {
-      dispatch(
-        actions.updateEffect({
-          effect: {
-            ...effect,
-            ...partial,
-            keyframes: newKFs
-              ? newKFs
-              : partial.keyframes
-              ? partial.keyframes
-              : effect.keyframes,
-          } as T,
-          stripId: strip.id,
-        })
-      );
+      allEffects.forEach((e) => {
+        const strip = effectIdToStripMap.get(e.id);
+        if (!strip) return;
+        dispatch(
+          actions.updateEffect({
+            effect: {
+              ...e,
+              ...partial,
+              keyframes: newKFs
+                ? newKFs
+                : partial.keyframes
+                ? partial.keyframes
+                : e.keyframes,
+            } as T,
+            stripId: strip.id,
+          })
+        );
+      });
     };
 
     const undo = () => {
