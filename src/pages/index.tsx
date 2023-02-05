c64f25b2f24888b2706efb5f6233f3e4bc4d8be0
toshusai <35446607+toshusai@users.noreply.github.com>
2023-02-05 11:54:19 +0900

    refactor: mv file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React from "react";
 import { Provider } from "react-redux";
 import styled from "styled-components";
 
@@ -13,8 +13,10 @@ import { SettingsMenuButton } from "@/components/SettingsMenuButton";
 import { StripPanel } from "@/components/strip_panel/StripPanel";
 import { Timeline } from "@/components/timeline_panel/Timeline";
 import store from "@/store";
-import { getDragHander } from "@/utils/getDragHander";
 import { initGlobalEvent } from "@/utils/initGlobalEvent";
+
+import { HPanel } from "./HPanel";
+import { VPanel } from "./VPanel";
 
 if (typeof window !== "undefined") {
   window.React = React;
@@ -48,24 +50,21 @@ const IndexPage = () => {
             overflow: "hidden",
           }}
         >
-          <VPanelBox2
+          <VPanel
             top={
-              <HPanelBox2
+              <HPanel
                 left={
-                  <VPanelBox2 top={<StripPanel />} bottom={<KeyFramePanel />} />
+                  <VPanel top={<StripPanel />} bottom={<KeyFramePanel />} />
                 }
                 right={<Preview />}
                 defaultRate={0.3}
               />
             }
             bottom={
-              <HPanelBox2
+              <HPanel
                 left={<Timeline />}
                 right={
-                  <HPanelBox2
-                    left={<AssetDetailsPanel />}
-                    right={<AssetPanel />}
-                  />
+                  <HPanel left={<AssetDetailsPanel />} right={<AssetPanel />} />
                 }
                 defaultRate={0.7}
               />
@@ -74,114 +73,6 @@ const IndexPage = () => {
         </div>
       </Provider>
     </>
-  );
-};
-
-const VPanelDivider = styled.div`
-  width: 100%;
-  min-height: 4px;
-  background-color: var(--color-panel-divider);
-  cursor: row-resize;
-  user-select: none;
-`;
-
-const HPanelDivider = styled.div`
-  min-width: 4px;
-  height: 100%;
-  background-color: var(--color-panel-divider);
-  cursor: col-resize;
-  user-select: none;
-`;
-
-const VPanelBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  width: 100%;
-`;
-const VPanelBox2: FC<{
-  top: React.ReactNode;
-  bottom: React.ReactNode;
-  defaultRate?: number;
-}> = (props) => {
-  const [rate, setRate] = React.useState(props.defaultRate ?? 0.5);
-  const topHeight = `calc(${(rate * 100).toFixed(0)}% - 2px)`;
-  const bottomHeight = `calc(${(1 - rate) * 100}% - 2px)`;
-  const handleMouseDown = getDragHander((ctx) => {
-    const { diffY } = ctx;
-    const el = ctx.startEvent.target as HTMLElement;
-    const ctxWidth = el.parentElement?.clientHeight ?? 0;
-    const newRate = Math.max(0, Math.min(1, rate + diffY / ctxWidth));
-    setRate(newRate);
-  });
-  return (
-    <VPanelBox>
-      <Panel2
-        style={{
-          height: topHeight,
-        }}
-      >
-        {props.top}
-      </Panel2>
-      <VPanelDivider onMouseDown={handleMouseDown} />
-      <Panel2
-        style={{
-          height: bottomHeight,
-        }}
-      >
-        {props.bottom}
-      </Panel2>
-    </VPanelBox>
-  );
-};
-
-const Panel2 = styled.div`
-  position: relative;
-  display: flex;
-  /* height: 100%; */
-  /* width: 100%; */
-`;
-
-const HPanelBox = styled.div`
-  display: flex;
-  flex-direction: row;
-  height: 100%;
-  width: 100%;
-`;
-
-const HPanelBox2: FC<{
-  left: React.ReactNode;
-  right: React.ReactNode;
-  defaultRate?: number;
-}> = (props) => {
-  const [rate, setRate] = React.useState(props.defaultRate ?? 0.5);
-  const leftWidth = `calc(${rate * 100}% - 2px)`;
-  const rightWidth = `calc(${(1 - rate) * 100}% - 2px)`;
-  const handleMouseDown = getDragHander((ctx) => {
-    const { diffX } = ctx;
-    const el = ctx.startEvent.target as HTMLElement;
-    const ctxWidth = el.parentElement?.clientWidth ?? 0;
-    const newRate = Math.max(0, Math.min(1, rate + diffX / ctxWidth));
-    setRate(newRate);
-  });
-  return (
-    <HPanelBox>
-      <Panel2
-        style={{
-          width: leftWidth,
-        }}
-      >
-        {props.left}
-      </Panel2>
-      <HPanelDivider onMouseDown={handleMouseDown} />
-      <Panel2
-        style={{
-          width: rightWidth,
-        }}
-      >
-        {props.right}
-      </Panel2>
-    </HPanelBox>
   );
 };
 
