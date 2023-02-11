48bf33d83410a0e2e547c60b9e61a22f053c4548
toshusai <toshusai.net@gmail.com>
2023-02-11 20:30:58 +0900

    fix: plugins

--- src/core/types/AppContext.ts
+++ src/core/types/AppContext.ts
@@ -1,8 +1,14 @@
+import { UseAnimationedValue, UseStripTime, UseUpdateEffect } from "./hooks";
 
 export interface AppContext {
   dispatch: any;
   actions: any;
   fs: {
     writeFile: (path: string, data: any) => Promise<any>;
+  };
+  hooks: {
+    useUpdateEffect: UseUpdateEffect;
+    useStripTime: UseStripTime;
+    useAnimationedValue: UseAnimationedValue;
   };
 }
--- src/core/types/hooks/index.ts
+++ src/core/types/hooks/index.ts
@@ -1,7 +1,7 @@
 import { Effect } from "../effects";
 import { Strip } from "../Strip";
 
-export type UseUpdateEffect<T extends Effect> = (
+export type UseUpdateEffect = <T extends Effect>(
   effect: T,
   strip: Strip
 ) => {
@@ -15,7 +15,7 @@ type PickProperties<T, TFilter> = {
   [K in keyof T as T[K] extends TFilter ? K : never]: T[K];
 };
 
-export type UseAnimationedValue<T extends Effect> = (
+export type UseAnimationedValue = <T extends Effect>(
   effect: T,
   strip: Strip
 ) => (key: keyof PickProperties<T, number | undefined>) => number;
