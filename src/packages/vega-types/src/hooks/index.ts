192eb6d653628e4f94a76bc729c3ded84de316b8
toshusai <35446607+toshusai@users.noreply.github.com>
2023-02-11 10:34:51 +0900

    feat: add types

---
+++ src/packages/vega-types/src/hooks/index.ts
@@ -0,0 +1,21 @@
+import { Effect } from "../effects";
+import { Strip } from "../Strip";
+
+export type UseUpdateEffect<T extends Effect> = (
+  effect: T,
+  strip: Strip
+) => {
+  emit: (partial: Partial<T>) => void;
+  addKeyFrame: (key: keyof T) => void;
+};
+
+export type UseStripTime = (strip: Strip) => number;
+
+type PickProperties<T, TFilter> = {
+  [K in keyof T as T[K] extends TFilter ? K : never]: T[K];
+};
+
+export type UseAnimationedValue<T extends Effect> = (
+  effect: T,
+  strip: Strip
+) => (key: keyof PickProperties<T, number | undefined>) => number;
