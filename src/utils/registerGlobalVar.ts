585a6cd493ff094d164336df91be61e3bf733654
toshusai <toshusai.net@gmail.com>
2023-07-29 19:49:51 +0900

    fix: path

--- src/utils/registerGlobalVar.ts
+++ src/utils/registerGlobalVar.ts
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 import * as Core from "@/core";
-import * as RiAppUi from "@/riapp-ui";
+import * as RiAppUi from "@/riapp-ui/src";
 
 export function registerGlobalVar() {
   if (typeof window !== "undefined") {
