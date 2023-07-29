e57a97e56b7d87ed3ab3ea055cd76d637d12e927
toshusai <35446607+toshusai@users.noreply.github.com>
2023-07-29 18:37:59 +0900

    feat: add image,video gizmo

--- src/components/preview_panel/utils/textEffectToRect.tsx
+++ src/components/preview_panel/utils/textEffectToRect.tsx
@@ -1,9 +1,51 @@
-import { TextEffect } from "@/core/types";
-import { calculateKeyFrameValue } from "@/core/types/utils/calculateKeyFrameValue";
 import {
-  measureMap,
-} from "@/rendering/updateTextEffect";
+  Effect,
+  ImageEffect,
+  isImageEffect,
+  isTextEffect,
+  isVideoEffect,
+  TextEffect,
+  VideoEffect,
+} from "@/core/types";
+import { calculateKeyFrameValue } from "@/core/types/utils/calculateKeyFrameValue";
+import { measureMap } from "@/rendering/updateTextEffect";
 import { SelectRectProps } from "@/riapp-ui/src";
+
+export function effectToRect(
+  effect: Effect,
+  scale: number,
+  left: number,
+  top: number,
+  currentTime: number,
+  fps: number
+) {
+  if (isTextEffect(effect)) {
+    return textEffectToRect(effect, scale, left, top, currentTime, fps);
+  }
+  if (isImageEffect(effect) || isVideoEffect(effect)) {
+    return imageEffectToRect(effect, scale, left, top, currentTime, fps);
+  }
+}
+
+function imageEffectToRect(
+  effect: ImageEffect | VideoEffect,
+  scale: number,
+  left: number,
+  top: number,
+  currentTime: number,
+  fps: number
+): SelectRectProps | null {
+  let { x, y } = effect;
+  x = calculateKeyFrameValue(effect.keyframes, currentTime, "x", effect.x, fps);
+  y = calculateKeyFrameValue(effect.keyframes, currentTime, "y", effect.y, fps);
+
+  return {
+    $left: x * scale + left,
+    $top: y * scale + top,
+    $width: (effect.width ?? 0) * scale,
+    $height: (effect.height ?? 0) * scale,
+  };
+}
 
 export function textEffectToRect(
   effect: TextEffect,
@@ -24,7 +66,8 @@ export function textEffectToRect(
   const lineBreaks = effect.text.split("\n").length - 1;
   return {
     $left: x * scale + left,
-    $top: y * scale + top - (measure.height - effect.fontSize * lineBreaks) * scale,
+    $top:
+      y * scale + top - (measure.height - effect.fontSize * lineBreaks) * scale,
     $width: measure.width * scale,
     $height: measure.height * scale,
   };
