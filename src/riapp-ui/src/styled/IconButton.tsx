5d006252b5770bd86fa2f003bf2f5e386b23a644
toshusai <35446607+toshusai@users.noreply.github.com>
2023-08-12 20:17:15 +0900

    feat: add max-height to IconButton

--- src/riapp-ui/src/styled/IconButton.tsx
+++ src/riapp-ui/src/styled/IconButton.tsx
@@ -9,6 +9,7 @@ export const IconButton = styled.button`
   width: 16px;
   min-width: 16px;
   min-height: 16px;
+  height: 16px;
   border: 1px solid var(${COLOR_BORDER_NAME});
   cursor: pointer;
   display: flex;
