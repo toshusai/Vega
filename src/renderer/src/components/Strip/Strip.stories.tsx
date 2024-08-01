8c0876b2fe0bdb0328a9ced30bd9663ff41f46ac
toshusai <toshusai.net@gmail.com>
2024-08-01 17:11:17 +0900

    feat: 時間スクロールを追加

--- src/renderer/src/components/Strip/Strip.stories.tsx
+++ src/renderer/src/components/Strip/Strip.stories.tsx
@@ -62,19 +62,20 @@ export const Default: Story = {
 import { proxy, useSnapshot } from 'valtio'
 import { createDragHandler } from '../../interactions/createDragHandler'
 import { SelectRect, useSelectRectHandler } from '@toshusai/cmpui'
+import { ScaleScrollBar } from '../ScaleScrollBar'
 
 const state = proxy({
   strips: [
-    { id: '1', layer: 0, left: 0, width: 100 },
-    { id: '2', layer: 1, left: 200, width: 100 }
+    { id: '1', layer: 0, left: 0, width: 1 },
+    { id: '2', layer: 1, left: 2, width: 1 }
   ],
   selectedIds: [] as string[]
 })
 
 const mainState = proxy({
   strips: [
-    { id: '1', layer: 0, left: 0, width: 100 },
-    { id: '2', layer: 1, left: 200, width: 100 }
+    { id: '1', layer: 0, left: 0, width: 1 },
+    { id: '2', layer: 1, left: 2, width: 1 }
   ],
   selectedIds: [] as string[]
 })
@@ -91,11 +92,17 @@ function checkCollision(
   )
 }
 
+const scaleState = proxy({
+  start: 0,
+  end: 1
+})
+
 export const Multiple: Story = {
   render: function Render() {
     const snap = useSnapshot(state)
     const mainSnap = useSnapshot(mainState)
     const { rect, onPointerDown } = useSelectRectHandler()
+    const scaleStateSnap = useSnapshot(scaleState)
 
     const refs = useRef([] as Array<HTMLDivElement | null>)
     const parent = useRef<HTMLDivElement | null>(null)
@@ -126,6 +133,10 @@ export const Multiple: Story = {
       state.selectedIds = hitIds
     }, [rect])
 
+    const pxPerSec = (1 / (scaleState.end - scaleState.start)) * 100
+    const length = 10
+    const startSec = scaleState.start * length
+
     return (
       <div
         style={{
@@ -136,150 +147,180 @@ export const Multiple: Story = {
           position: 'relative',
           overflow: 'hidden'
         }}
-        ref={parent}
-        onPointerDown={(e) => {
-          state.selectedIds = []
-          onPointerDown(e)
-        }}
       >
-        {rect && <SelectRect {...rect} />}
-        {snap.strips.map((strip, i) => {
-          function isInvalid(id: string) {
-            if (!state.selectedIds.includes(id)) return false
-            const currentStrip = state.strips.find((strip) => strip.id === id)
-            if (!currentStrip) return true
-            const sameLayerStrips = state.strips.filter(
-              (strip) => currentStrip.layer === strip.layer
-            )
-            if (sameLayerStrips.length === 1) return false
+        <div
+          style={{
+            width: '100%',
+            height: '128px',
+            display: 'flex',
+            position: 'relative',
+            overflow: 'hidden'
+          }}
+          ref={parent}
+          onPointerDown={(e) => {
+            state.selectedIds = []
+            onPointerDown(e)
+          }}
+        >
+          {rect && <SelectRect {...rect} />}
+          {snap.strips.map((strip, i) => {
+            function isInvalid(id: string) {
+              if (!state.selectedIds.includes(id)) return false
+              const currentStrip = state.strips.find((strip) => strip.id === id)
+              if (!currentStrip) return true
+              const sameLayerStrips = state.strips.filter(
+                (strip) => currentStrip.layer === strip.layer
+              )
+              if (sameLayerStrips.length === 1) return false
 
-            const sortedStrips = sameLayerStrips.sort((a, b) => a.left - b.left)
-            const index = sortedStrips.findIndex((strip) => strip.id === id)
-            if (index === 0)
-              return currentStrip.left + currentStrip.width > sortedStrips[index + 1].left
-            if (index === sortedStrips.length - 1)
+              const sortedStrips = sameLayerStrips.sort((a, b) => a.left - b.left)
+              const index = sortedStrips.findIndex((strip) => strip.id === id)
+              if (index === 0)
+                return currentStrip.left + currentStrip.width > sortedStrips[index + 1].left
+              if (index === sortedStrips.length - 1)
+                return (
+                  currentStrip.left < sortedStrips[index - 1].left + sortedStrips[index - 1].width
+                )
+
               return (
+                currentStrip.left + currentStrip.width > sortedStrips[index + 1].left ||
                 currentStrip.left < sortedStrips[index - 1].left + sortedStrips[index - 1].width
               )
+            }
+
+            function getSnapPoints(ids: string[]): number[] {
+              const otherStrips = state.strips.filter((strip) => !ids.includes(strip.id))
+              return otherStrips
+                .flatMap((strip) => [strip.left, strip.left + strip.width])
+                .sort((a, b) => a - b)
+            }
+
+            const invalid = isInvalid(strip.id)
 
             return (
-              currentStrip.left + currentStrip.width > sortedStrips[index + 1].left ||
-              currentStrip.left < sortedStrips[index - 1].left + sortedStrips[index - 1].width
-            )
-          }
-
-          function getSnapPoints(ids: string[]): number[] {
-            const otherStrips = state.strips.filter((strip) => !ids.includes(strip.id))
-            return otherStrips
-              .flatMap((strip) => [strip.left, strip.left + strip.width])
-              .sort((a, b) => a - b)
-          }
-
-          const invalid = isInvalid(strip.id)
-
-          return (
-            <Strip
-              ref={(el) => {
-                refs.current[i] = el
-                if (el) {
-                  el.id = strip.id
-                }
-              }}
-              key={i}
-              invalid={invalid}
-              top={strip.layer * 32 + 1 + 2 * strip.layer}
-              selected={snap.selectedIds.includes(strip.id)}
-              left={strip.left}
-              width={strip.width}
-              onChange={(left, width) => {
-                const snapPoints = getSnapPoints(state.selectedIds)
-                const isChangedRight = left === strip?.left
-                const isChangedLeft = width === strip?.width
-
-                const { value: snappedLeft, isSnapped } = checkSnap(left, snapPoints)
-                const snappedLeftDiff = snappedLeft - left
-                if (isSnapped) {
-                  left = snappedLeft
-                  if (!isChangedLeft) {
-                    width -= snappedLeftDiff
+              <Strip
+                ref={(el) => {
+                  refs.current[i] = el
+                  if (el) {
+                    el.id = strip.id
                   }
-                }
+                }}
+                key={i}
+                invalid={invalid}
+                top={strip.layer * 32 + 1 + 2 * strip.layer}
+                selected={snap.selectedIds.includes(strip.id)}
+                left={strip.left * pxPerSec - startSec * pxPerSec}
+                width={strip.width * pxPerSec}
+                onChange={(left, width) => {
+                  left += startSec * pxPerSec
+                  const snapPoints = getSnapPoints(state.selectedIds).map(
+                    (point) => point * pxPerSec
+                  )
+                  const isChangedRight = left === strip?.left * pxPerSec
+                  const isChangedLeft = width === strip?.width * pxPerSec
 
-                const { value: snappedRight, isSnapped: isWidthSnapped } = checkSnap(
-                  left + width,
-                  snapPoints
-                )
-                const snappedRightDiff = snappedRight - (left + width)
-                if (!isChangedRight) {
-                  if (!isSnapped && isWidthSnapped) {
-                    left = left + snappedRightDiff
+                  const { value: snappedLeft, isSnapped } = checkSnap(left, snapPoints)
+                  const snappedLeftDiff = snappedLeft - left
+                  if (isSnapped) {
+                    left = snappedLeft
+                    if (!isChangedLeft) {
+                      width -= snappedLeftDiff
+                    }
                   }
-                } else if (isWidthSnapped) {
-                  width = width + snappedRightDiff
-                }
 
-                const diffLeft = left - state.strips[i].left
-                const diffWidth = width - state.strips[i].width
-
-                state.strips.forEach((strip, j) => {
-                  if (state.selectedIds.includes(strip.id)) {
-                    state.strips[j].left += diffLeft
-                    state.strips[j].width += diffWidth
+                  const { value: snappedRight, isSnapped: isWidthSnapped } = checkSnap(
+                    left + width,
+                    snapPoints
+                  )
+                  const snappedRightDiff = snappedRight - (left + width)
+                  if (!isChangedRight) {
+                    if (!isSnapped && isWidthSnapped) {
+                      left = left + snappedRightDiff
+                    }
+                  } else if (isWidthSnapped) {
+                    width = width + snappedRightDiff
                   }
-                })
-              }}
-              onPointerDown={(e) => {
-                if (e.metaKey) {
-                  state.selectedIds.push(strip.id)
-                } else if (!state.selectedIds.includes(strip.id)) {
-                  state.selectedIds = [strip.id]
-                }
 
-                const prevSelectedIds = snap.selectedIds.length
-                onClickFromPointerDown(() => {
-                  if (prevSelectedIds == state.selectedIds.length) {
+                  const diffLeft = left - state.strips[i].left * pxPerSec
+                  const diffWidth = width - state.strips[i].width * pxPerSec
+
+                  state.strips.forEach((strip, j) => {
+                    if (state.selectedIds.includes(strip.id)) {
+                      state.strips[j].left += diffLeft / pxPerSec
+                      state.strips[j].width += diffWidth / pxPerSec
+                    }
+                  })
+                }}
+                onPointerDown={(e) => {
+                  if (e.metaKey) {
+                    state.selectedIds.push(strip.id)
+                  } else if (!state.selectedIds.includes(strip.id)) {
                     state.selectedIds = [strip.id]
                   }
-                })
-                createDragHandler({
-                  onDown: (e) => {
-                    return {
-                      offsetY: e.nativeEvent.offsetY,
-                      currentLayers: snap.strips.map((strip) => strip.layer)
+
+                  const prevSelectedIds = snap.selectedIds.length
+                  onClickFromPointerDown(() => {
+                    if (prevSelectedIds == state.selectedIds.length) {
+                      state.selectedIds = [strip.id]
                     }
-                  },
-                  onMove: (_, ctx, move) => {
-                    if (!ctx) return
-                    const { offsetY, currentLayers } = ctx
-                    state.selectedIds.forEach((id) => {
-                      const i = snap.strips.findIndex((strip) => strip.id === id)
-                      const newLayer = currentLayers[i] + Math.ceil((move.diffY + offsetY) / 32) - 1
-                      if (newLayer < 0) return
-                      state.strips[i].layer = newLayer
-                    })
+                  })
+                  createDragHandler({
+                    onDown: (e) => {
+                      return {
+                        offsetY: e.nativeEvent.offsetY,
+                        currentLayers: snap.strips.map((strip) => strip.layer)
+                      }
+                    },
+                    onMove: (_, ctx, move) => {
+                      if (!ctx) return
+                      const { offsetY, currentLayers } = ctx
+                      state.selectedIds.forEach((id) => {
+                        const i = snap.strips.findIndex((strip) => strip.id === id)
+                        const newLayer =
+                          currentLayers[i] + Math.ceil((move.diffY + offsetY) / 32) - 1
+                        if (newLayer < 0) return
+                        state.strips[i].layer = newLayer
+                      })
+                    }
+                  })(e as React.PointerEvent<HTMLElement>)
+                }}
+                onChangeEnd={() => {
+                  const invalid = isInvalid(strip.id)
+                  if (invalid) {
+                    state.strips = mainSnap.strips.map((strip) => ({ ...strip }))
+                  } else {
+                    mainState.strips = state.strips.map((strip) => ({ ...strip }))
                   }
-                })(e as React.PointerEvent<HTMLElement>)
-              }}
-              onChangeEnd={() => {
-                const invalid = isInvalid(strip.id)
-                if (invalid) {
-                  state.strips = mainSnap.strips.map((strip) => ({ ...strip }))
-                } else {
-                  mainState.strips = snap.strips.map((strip) => ({ ...strip }))
-                }
-              }}
-            >
-              <div
-                style={{
-                  padding: '0 4px',
-                  userSelect: 'none'
                 }}
               >
-                Strip
-              </div>
-            </Strip>
-          )
-        })}
+                <div
+                  style={{
+                    padding: '0 4px',
+                    userSelect: 'none'
+                  }}
+                >
+                  Strip
+                </div>
+              </Strip>
+            )
+          })}
+        </div>
+        <div
+          style={{
+            position: 'absolute',
+            bottom: 0,
+            width: '100%'
+          }}
+        >
+          <ScaleScrollBar
+            end={scaleStateSnap.end}
+            start={scaleStateSnap.start}
+            onChange={(start, end) => {
+              scaleState.start = start
+              scaleState.end = end
+            }}
+          />
+        </div>
       </div>
     )
   }
