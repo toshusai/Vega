0b6aec310c07ffcad102c2f9426188d5d85e92f0
toshusai <35446607+toshusai@users.noreply.github.com>
2024-08-01 13:57:17 +0900

    feat: Stripを追加

---
+++ src/renderer/src/components/Strip/index.tsx
@@ -0,0 +1,111 @@
+import './index.css'
+
+import { createDragHandler } from '../../interactions/createDragHandler'
+import React from 'react'
+
+type StripProps = {
+  children: React.ReactNode
+  left: number
+  width: number
+  onChange: (left: number, width: number) => void
+  onChangeEnd: (left: number, width: number) => void
+}
+
+const MIN_WIDTH = 8
+
+export function Strip({ left, width, children, onChange, onChangeEnd }: StripProps) {
+  const handleLeftHandle = createDragHandler({
+    onDown: (e) => {
+      e.stopPropagation()
+      return {
+        left: left,
+        width: width
+      }
+    },
+    onMove: (_, ctx, move) => {
+      if (!ctx) return
+      const { left, width } = ctx
+      let newLeft = left + move.diffX
+      let newWidth = width - move.diffX
+      if (newWidth < MIN_WIDTH) {
+        newWidth = MIN_WIDTH
+        newLeft = left + width - MIN_WIDTH
+      }
+
+      onChange(newLeft, newWidth)
+      return {
+        left: newLeft,
+        width: newWidth
+      }
+    },
+    onUp: (_, __, moveCtx) => {
+      if (!moveCtx) return
+      const { left, width } = moveCtx
+      onChangeEnd(left, width)
+    }
+  })
+
+  const handleRightHandle = createDragHandler({
+    onDown: (e) => {
+      e.stopPropagation()
+      return {
+        left: left,
+        width: width
+      }
+    },
+    onMove: (_, ctx, move) => {
+      if (!ctx) return
+      const { left, width } = ctx
+      const newWidth = Math.max(width + move.diffX, MIN_WIDTH)
+      onChange(left, newWidth)
+    },
+    onUp: (_, __, moveCtx) => {
+      if (!moveCtx) return
+      const { left, width } = moveCtx
+      onChangeEnd(left, width)
+    }
+  })
+
+  const handleMove = createDragHandler({
+    onDown: () => {
+      return {
+        left: left,
+        width: width
+      }
+    },
+    onMove: (_, ctx, move) => {
+      if (!ctx) return
+      const { left, width } = ctx
+      const newLeft = left + move.diffX
+      onChange(newLeft, width)
+    },
+    onUp: (_, __, moveCtx) => {
+      if (!moveCtx) return
+      const { left, width } = moveCtx
+      onChange(left, width)
+    }
+  })
+
+  return (
+    <div
+      className="strip"
+      style={{
+        left: left,
+        width: width
+      }}
+      onPointerDown={handleMove}
+    >
+      <Handle onPointerDown={handleLeftHandle} style={{ left: 0 }} />
+      <div className="strip-container">{children}</div>
+      <Handle onPointerDown={handleRightHandle} style={{ right: 0 }} />
+    </div>
+  )
+}
+
+function Handle(props: React.ComponentProps<'div'>) {
+  return (
+    <div className="handle" {...props}>
+      <div></div>
+    </div>
+  )
+}
---
+++ src/renderer/src/interactions/createDragHandler.ts
@@ -0,0 +1,93 @@
+import { useMemo } from 'react'
+
+export type OnMoveHandler<T = undefined, U = undefined> = (
+  e: PointerEvent,
+  ctx: T | undefined,
+  move: {
+    diffX: number
+    diffY: number
+    deltaX: number
+    deltaY: number
+  }
+) => U
+
+export function createDragHandler<T, U>({
+  onDown,
+  onMove,
+  onUp,
+  options
+}: {
+  /**
+   * if returns false, the drag will be canceled
+   */
+  onDown?: (e: React.PointerEvent<HTMLElement | SVGElement>) => T | false
+  onMove?: OnMoveHandler<T, U>
+  onUp?: (e: PointerEvent, downCtx?: T, moveCtx?: U, moveEvent?: PointerEvent) => void
+  options?: {
+    disableCapture?: boolean
+  }
+}) {
+  const handlePointerDown = (downEvent: React.PointerEvent<HTMLElement | SVGElement>) => {
+    if (downEvent.button !== 0) return
+    const el = downEvent.currentTarget
+
+    const downCtx = onDown?.(downEvent)
+    let prevEvent: PointerEvent | undefined = undefined
+    let moveCtx: U | undefined = undefined
+    const handlePointerMove = (e: Event) => {
+      if (!(e instanceof PointerEvent)) return
+      if (downCtx === false) return
+      e.preventDefault()
+      moveCtx = onMove?.(e, downCtx, {
+        diffX: e.clientX - downEvent.clientX,
+        diffY: e.clientY - downEvent.clientY,
+        deltaX: e.clientX - (prevEvent?.clientX ?? downEvent.clientX),
+        deltaY: e.clientY - (prevEvent?.clientY ?? downEvent.clientY)
+      })
+
+      prevEvent = e
+    }
+
+    const handlePointerUp = (e: Event) => {
+      if (!(e instanceof PointerEvent)) return
+      if (downCtx === false) return
+      el.removeEventListener('pointermove', handlePointerMove)
+      window.removeEventListener('pointerup', handlePointerUp)
+      if (options?.disableCapture !== true) {
+        el.releasePointerCapture(e.pointerId)
+      }
+      onUp?.(e, downCtx, moveCtx, prevEvent)
+    }
+
+    if (downCtx === false) return
+
+    if (options?.disableCapture !== true) {
+      el.setPointerCapture(downEvent.pointerId)
+    }
+    el.addEventListener('pointermove', handlePointerMove, {
+      passive: false
+    })
+    window.addEventListener('pointerup', handlePointerUp)
+  }
+
+  return handlePointerDown
+}
+
+export function useCreateDragHandler<T, U>({
+  onDown,
+  onMove,
+  onUp,
+  options
+}: {
+  onDown?: (e: React.PointerEvent<HTMLElement | SVGElement>) => T | false
+  onMove?: OnMoveHandler<T, U>
+  onUp?: (e: PointerEvent, downCtx?: T, moveCtx?: U, moveEvent?: PointerEvent) => void
+  options?: {
+    disableCapture?: boolean
+  }
+}) {
+  return useMemo(
+    () => createDragHandler({ onDown, onMove, onUp, options }),
+    [onDown, onMove, onUp, options]
+  )
+}
