f26e70387a24331226a1774084ac3be7a7b7a199
toshusai <toshusai.net@gmail.com>
2024-08-01 12:47:18 +0900

    feat: ベースのレイアウトを追加

---
+++ src/renderer/src/components/Panel/index.tsx
@@ -0,0 +1,10 @@
+import './index.css'
+
+export function Panel() {
+  return (
+    <div className="panel--root">
+      <div className="panel--header"></div>
+      <div className="panel--content"></div>
+    </div>
+  )
+}
