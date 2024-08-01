7703cf91b526052545a0273f9add5a5703c32d88
toshusai <toshusai.net@gmail.com>
2024-08-01 14:40:27 +0900

    feat: 複数選択を追加

--- src/renderer/src/components/Strip/Strip.stories.tsx
+++ src/renderer/src/components/Strip/Strip.stories.tsx
@@ -59,3 +59,95 @@ export const Default: Story = {
     )
   }
 }
+import { proxy, useSnapshot } from 'valtio'
+
+const state = proxy({
+  strips: [
+    { id: '1', layer: 0, left: 0, width: 100 },
+    { id: '2', layer: 1, left: 200, width: 100 }
+  ],
+  selectedIds: [] as string[]
+})
+
+export const Multiple: Story = {
+  render: function Render() {
+    const snap = useSnapshot(state)
+
+    return (
+      <div
+        style={{
+          width: '100%',
+          height: '128px',
+          display: 'flex',
+          border: '1px solid black',
+          position: 'relative',
+          overflow: 'hidden'
+        }}
+        onPointerDown={() => {
+          state.selectedIds = []
+        }}
+      >
+        {snap.strips.map((strip, i) => {
+          return (
+            <Strip
+              key={i}
+              top={strip.layer * 32 + 1 + 2 * i}
+              selected={snap.selectedIds.includes(strip.id)}
+              left={strip.left}
+              width={strip.width}
+              onChange={(left, width) => {
+                const diffLeft = left - state.strips[i].left
+                const diffWidth = width - state.strips[i].width
+                state.strips.forEach((strip, j) => {
+                  if (state.selectedIds.includes(strip.id)) {
+                    state.strips[j].left += diffLeft
+                    state.strips[j].width += diffWidth
+                  }
+                })
+              }}
+              onPointerDown={(e) => {
+                if (e.metaKey) {
+                  state.selectedIds.push(strip.id)
+                } else if (!state.selectedIds.includes(strip.id)) {
+                  state.selectedIds = [strip.id]
+                }
+
+                const prevSelectedIds = snap.selectedIds.length
+                onClickFromPointerDown(() => {
+                  if (prevSelectedIds == state.selectedIds.length) {
+                    state.selectedIds = [strip.id]
+                  }
+                })
+              }}
+              onChangeEnd={() => {}}
+            >
+              <div
+                style={{
+                  padding: '0 4px'
+                }}
+              >
+                Strip
+              </div>
+            </Strip>
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+function onClickFromPointerDown(callback: () => void) {
+  let isMoved = false
+  const handleMove = () => {
+    isMoved = true
+  }
+
+  const handleUp = () => {
+    window.removeEventListener('pointerup', handleUp)
+    window.removeEventListener('pointermove', handleMove)
+    if (!isMoved) callback()
+  }
+
+  window.addEventListener('pointermove', handleMove)
+  window.addEventListener('pointerup', handleUp)
+}
