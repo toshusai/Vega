b6ca9b0545a3d9910f39a223785f06348f633d4b
toshusai <35446607+toshusai@users.noreply.github.com>
2024-08-01 12:47:18 +0900

    feat: ベースのレイアウトを追加

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,14 @@
+import { SplitPane } from '@toshusai/cmpui'
+import '@toshusai/cmpui/dist/index.css'
+import { Panel } from './components/Panel'
+
 function App() {
-  return null
+  return (
+    <SplitPane type="vertical" sizes={['60%', '40%']}>
+      <Panel />
+      <Panel />
+    </SplitPane>
+  )
 }
 
 export default App
--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,3 +1,5 @@
+import './global.css'
+
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import { StrictMode } from 'react'
