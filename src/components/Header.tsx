1fca8261c63127f2dbe209ba9ea2bd6643c70a45
toshusai <toshusai.net@gmail.com>
2024-09-06 10:50:41 +0900

    feat: add file io

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,7 +18,46 @@ export function Header() {
                   state.recordingState = 'recording'
                 }}
               >
-                Export
+                Export the video
+              </MenuBarItem>
+              <MenuBarItem
+                onClick={() => {
+                  const input = document.createElement('input')
+                  input.type = 'file'
+                  input.accept = '.json'
+                  input.onchange = (e) => {
+                    const target = e.target as HTMLInputElement
+                    const file = target.files?.[0]
+                    if (file) {
+                      const reader = new FileReader()
+                      reader.onload = (e) => {
+                        const json = e.target?.result as string
+                        const obj = JSON.parse(json)
+                        Object.assign(state, obj)
+                      }
+                      reader.readAsText(file)
+                    }
+                  }
+                  input.click()
+                  input.remove()
+                }}
+              >
+                Open the project file
+              </MenuBarItem>
+
+              <MenuBarItem
+                onClick={() => {
+                  const json = JSON.stringify(state, null, 4)
+                  const blob = new Blob([json], { type: 'application/json' })
+                  const url = URL.createObjectURL(blob)
+                  const a = document.createElement('a')
+                  a.href = url
+                  a.download = 'vega.json'
+                  a.click()
+                  a.remove()
+                }}
+              >
+                Download the project file
               </MenuBarItem>
             </>
           }
